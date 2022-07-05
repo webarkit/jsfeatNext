5dbe07a54b4938855c4d1991c65bd7ae35881798
kalwalt <github@kalwaltart.it>
2022-07-05 21:10:18 +0200

    make some constants static

--- src/jsfeatNext.js
+++ src/jsfeatNext.js
@@ -10,21 +10,21 @@ export default class jsfeatNext {
     // CONSTANTS
     static EPSILON = 0.0000001192092896;
     static FLT_MIN = 1E-37;
-    U8_t = 0x0100;
-    S32_t = 0x0200;
-    F32_t = 0x0400;
-    S64_t = 0x0800;
-    F64_t = 0x1000;
-    C1_t = 0x01;
-    C2_t = 0x02;
-    C3_t = 0x03;
-    C4_t = 0x04;
+    static U8_t = 0x0100;
+    static S32_t = 0x0200;
+    static F32_t = 0x0400;
+    static S64_t = 0x0800;
+    static F64_t = 0x1000;
+    static C1_t = 0x01;
+    static C2_t = 0x02;
+    static C3_t = 0x03;
+    static C4_t = 0x04;
 
     // color conversion
-    COLOR_RGBA2GRAY = 0;
-    COLOR_RGB2GRAY = 1;
-    COLOR_BGRA2GRAY = 2;
-    COLOR_BGR2GRAY = 3;
+    static COLOR_RGBA2GRAY = 0;
+    static COLOR_RGB2GRAY = 1;
+    static COLOR_BGRA2GRAY = 2;
+    static COLOR_BGR2GRAY = 3;
 
     // box blur option
     BOX_BLUR_NOSCALE = 0x01;
