a4909c9e3308763bfeb64627086937b154a216cd
kalwalt <github@kalwaltart.it>
2022-07-07 17:43:01 +0200

    new linalg and cache classes

---
+++ src/linalg/linalg-base.js
@@ -0,0 +1,19 @@
+export function swap(A, i0, i1, t) {
+    t = A[i0];
+    A[i0] = A[i1];
+    A[i1] = t;
+}
+
+export function hypot(a, b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if( a > b ) {
+        b /= a;
+        return a*Math.sqrt(1.0 + b*b);
+    }
+    if( b > 0 ) {
+        a /= b;
+        return b*Math.sqrt(1.0 + a*a);
+    }
+    return 0.0;
+}
