f2962249969556b85e17b9c530b0585c1f2a3128
kalwalt <github@kalwaltart.it>
2022-07-25 16:42:33 +0200

    yape06 converted to typescript

---
+++ types/src_ts/yape06/yape06_utils.d.ts
@@ -0,0 +1,2 @@
+export declare function compute_laplacian(src: any[], dst: any[], w: number, h: any, Dxx: number, Dyy: number, sx: number, sy: number, ex: number, ey: number): void;
+export declare function hessian_min_eigen_value(src: number[], off: number, tr: number, Dxx: number, Dyy: number, Dxy: number, Dyx: number): number;
