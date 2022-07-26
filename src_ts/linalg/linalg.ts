6ba4785fc06f84becb9a2d14f184983282cd5c5c
kalwalt <github@kalwaltart.it>
2022-07-26 16:44:51 +0200

    set return types for linalg and math methods

--- src_ts/linalg/linalg.ts
+++ src_ts/linalg/linalg.ts
@@ -1,9 +1,9 @@
 import {matrix_t} from '../matrix_t/matrix_t';
 export class linalg {
-    eigenVV(A: matrix_t, vects: matrix_t, vals?: { data: { [x: string]: any } }) {
+    eigenVV(A: matrix_t, vects: matrix_t, vals?: { data: { [x: string]: any } }): void {
         throw new Error('Method not implemented.');
     }
-    lu_solve(A: matrix_t, B: { data: any }) {
+    lu_solve(A: matrix_t, B: { data: any }): number {
         throw new Error('Method not implemented.');
     }
 }
