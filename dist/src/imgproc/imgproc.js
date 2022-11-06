0593fe1b1a1bd5ae8757a740e87c16576ac85881
kalwalt <info@kalwaltart.it>
2022-11-06 20:22:13 +0100

    added other imgproc type defs see #13

--- dist/src/imgproc/imgproc.js
+++ dist/src/imgproc/imgproc.js
@@ -2,13 +2,43 @@ export class imgproc {
     grayscale(src, w, h, dst, code) {
         throw new Error('Method not implemented.');
     }
+    resample(src, dst, nw, nh) {
+        throw new Error('Method not implemented.');
+    }
+    box_blur_gray(src, dst, radius, options) {
+        throw new Error('Method not implemented.');
+    }
+    gaussian_blur(src, dst, kernel_size, sigma) {
+        throw new Error('Method not implemented.');
+    }
+    hough_transform(img, rho_res, theta_res, threshold) {
+        throw new Error('Method not implemented.');
+    }
     pyrdown(src, dst, sx, sy) {
         throw new Error('Method not implemented.');
     }
     scharr_derivatives(src, dst) {
         throw new Error('Method not implemented.');
     }
+    sobel_derivatives(src, dst) {
+        throw new Error('Method not implemented.');
+    }
+    compute_integral_image(src, dst_sum, dst_sqsum, dst_tilted) {
+        throw new Error('Method not implemented.');
+    }
+    equalize_histogram(src, dst) {
+        throw new Error('Method not implemented.');
+    }
+    canny(src, dst, low_thresh, high_thresh) {
+        throw new Error('Method not implemented.');
+    }
+    warp_perspective(src, dst, transform, fill_value) {
+        throw new Error('Method not implemented.');
+    }
     warp_affine(src, dst, transform, fill_value) {
+        throw new Error('Method not implemented.');
+    }
+    skindetector(src, dst) {
         throw new Error('Method not implemented.');
     }
 }
