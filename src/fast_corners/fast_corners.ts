da92ac4c1a24f1d54280031c4111c3925289225d
kalwalt <info@kalwaltart.it>
2022-11-08 14:55:07 +0100

    fast_corners type defs

--- src/fast_corners/fast_corners.ts
+++ src/fast_corners/fast_corners.ts
@@ -1,1 +1,10 @@
-export class fast_corners {}
+import { matrix_t } from '../matrix_t/matrix_t'
+import { point_t } from '../point_t/point_t'
+export class fast_corners {
+  set_threshold(threshold: number): number {
+    throw new Error('Method not implemented.');
+  }
+  detect(src: matrix_t, corners: point_t[], border: number): number {
+    throw new Error('Method not implemented.');
+  }
+}
