1a7c20af4038ffc3ab0f8b82d54e6a978ec04b01
kalwalt <info@kalwaltart.it>
2022-11-08 16:15:57 +0100

    type defs for optical_flow_lk

--- src/optical_flow_lk/optical_flow_lk.ts
+++ src/optical_flow_lk/optical_flow_lk.ts
@@ -1,1 +1,6 @@
-export class optical_flow_lk {}
+import { pyramid_t } from '../pyramid_t/pyramid_t'
+export class optical_flow_lk {
+  track(prev_pyr: pyramid_t, curr_pyr: pyramid_t, prev_xy: Float32Array, curr_xy: Float32Array, count: number, win_size: number, max_iter: number, status: Uint8Array, eps: number, min_eigen_threshold: number): void {
+    throw new Error('Method not implemented.');
+  }
+}
