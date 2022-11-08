fc0840337ce35e81a64a8ce3929d7c0df8c46765
kalwalt <info@kalwaltart.it>
2022-11-08 16:15:57 +0100

    type defs for optical_flow_lk

--- types/src/optical_flow_lk/optical_flow_lk.d.ts
+++ types/src/optical_flow_lk/optical_flow_lk.d.ts
@@ -1,2 +1,4 @@
+import { pyramid_t } from '../pyramid_t/pyramid_t';
 export declare class optical_flow_lk {
+    track(prev_pyr: pyramid_t, curr_pyr: pyramid_t, prev_xy: Float32Array, curr_xy: Float32Array, count: number, win_size: number, max_iter: number, status: Uint8Array, eps: number, min_eigen_threshold: number): void;
 }
