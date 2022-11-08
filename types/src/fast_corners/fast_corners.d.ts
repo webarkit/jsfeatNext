d50c2c1a6a1276c67363fddcb460b9fd49f089de
kalwalt <info@kalwaltart.it>
2022-11-08 14:55:07 +0100

    fast_corners type defs

--- types/src/fast_corners/fast_corners.d.ts
+++ types/src/fast_corners/fast_corners.d.ts
@@ -1,2 +1,6 @@
+import { matrix_t } from '../matrix_t/matrix_t';
+import { point_t } from '../point_t/point_t';
 export declare class fast_corners {
+    set_threshold(threshold: number): number;
+    detect(src: matrix_t, corners: point_t[], border: number): number;
 }
